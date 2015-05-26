9ed5a99928f7dc1f6b2d72ebc528cd0d1715fee9
hmsajjad <hmsajjad89@gmail.com>
2015-05-26 11:20:37 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sajjad on 26/05/2015.
+ */
+var a = 1;
+alert(a);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by sajjad on 26/05/2015.
+ */
+var a = 1;
+alert(a);
